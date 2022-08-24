f66a929d0858b8a01ba7b2d346d81398d9334e9f
MICHAEL WELSH <u0500311@umail.utah.edu>
2022-08-23 21:02:46 -0600

    Add project files.

---
+++ FinalsSim/Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+// Result list indices: H4, H5, H6, H7, A4, A5, A6, A7
+// Hometeam wins are first four indices and how many games the series ran. Example: 
+// index zero is how many series simulations resulted in a sweep for the team with
+// home court advantage. 
+using FinalsSim;
+
+List<int> result = new() { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+string HomeTeam = "Golden State";
+string AwayTeam = "Boston";
+
+Console.WriteLine("\t\tCalculating Series Odds... ");
+Console.WriteLine();
+Console.WriteLine();
+// Predicted probability of team with homecourt advantage winning a home game. 
+// (derived from neural net and rounded
+// S
+// 2022 finals probabilities: Home - 54%, Away - 52%
+if (args.Length != 2)
+    throw new ArgumentException("Incorrect number of arguments.");
+
+// double HomePercentage = Double.Parse(args[0]);
+double HomePercentage = 0.54;
+
+// Predicted probability of team without homecourt advantage winning a home game. 
+// double AwayPercentage = Double.Parse(args[1]);
+double AwayPercentage = 0.52;
+
+SeriesSim sim = new SeriesSim(AwayPercentage, HomePercentage);
+int runs = 10_000_000;
+
+// Run series simulation N times and count results
+for (int i = 0; i < runs; i++)
+{
+    int index = sim.Run();
+    result[index]++;
+}
+
+//int mostLikely = result.IndexOf(result.Max());
+//double percent = (double)(result.Max() / 10000.0) * 100.0;
+
+//if(mostLikely < 4)
+//    Console.WriteLine($"There is a {percent} percent chance of {HomeTeam} winning the series in {mostLikely+4} games.");
+//else
+//    Console.WriteLine($"There is a {percent} percent chance of {AwayTeam} winning the series in {mostLikely} games.");
+
+double homeTeamProb = 0;
+double awayTeamProb = 0;
+
+for (int i = 0; i < result.Count; i++)
+{
+    double prob = (result[i] / (double)runs) * 100.0;
+    string winner = (i < 4) ? HomeTeam : AwayTeam;
+    int games = (i < 4) ? i + 4 : i;
+    Console.WriteLine($"There is a {prob} percent chance of {winner} winning the series in {games} games.");
+    Console.WriteLine();
+
+    if(i < 4)
+        homeTeamProb += result[i];
+    else awayTeamProb += result[i];
+}
+
+homeTeamProb /= runs;
+awayTeamProb /= runs;
+
+Console.WriteLine($"{HomeTeam} has a {homeTeamProb * 100} percent chance of winning the series.");
+Console.WriteLine();
+Console.WriteLine($"{AwayTeam} has a {awayTeamProb * 100} percent chance of winning the series.");
+
+
+
---
+++ FinalsSim/SeriesSim.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalsSim
+{
+    
+    internal class SeriesSim
+    {
+     
+        public double AwayProb { get; set; }
+        public double HomeProb { get; set; }
+
+        public int HomeWins { get; set; }
+        public int AwayWins { get; set; }
+
+
+        
+
+
+        public SeriesSim(double awayProb, double homeProb)
+        {
+            AwayProb = awayProb;
+            HomeProb = homeProb;
+            HomeWins = 0;
+            AwayWins = 0;
+        }
+
+        /*
+         * Runs a 7 game series 
+         * 
+         * Returns the index of the result in an 8 element array containing all possible
+         * outcomes.
+         */
+        public int Run()
+        {
+            // Reset series wins
+            this.HomeWins = 0;
+            this.AwayWins = 0;
+
+            // Run the first four games
+            gameSim('h');
+            gameSim('h');
+            gameSim('a');
+            gameSim('a');
+
+            // Run Next three games if necessary
+            if (HomeWins != 4 && AwayWins != 4)
+                gameSim('h');
+            if (HomeWins != 4 && AwayWins != 4)
+                gameSim('a');
+            if (HomeWins != 4 && AwayWins != 4)
+                gameSim('h');
+
+            if (HomeWins > AwayWins)
+                return HomeWins + AwayWins - 4;
+            
+            return HomeWins + AwayWins;
+        }
+
+        /*
+         * Predicts the result of a single game using the field probabilities. 
+         * User must specify type of game ('h' or 'a' for home or away).
+         * 
+         * 
+         */
+        private void gameSim(char type)
+        {
+            
+            if(type == 'h')
+            {
+                Random rng = new Random();
+                double predict = rng.NextDouble();
+                if (predict <= HomeProb)
+                    this.HomeWins++;
+                else this.AwayWins++;
+            }
+            if(type == 'a')
+            {
+                Random rng = new Random();
+                double predict = rng.NextDouble();
+                if (predict <= AwayProb)
+                    this.AwayWins++;
+                else this.HomeWins++;
+            }
+
+        }
+    }
+}
