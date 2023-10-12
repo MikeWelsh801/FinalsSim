f994050bac0bd05bceb2934e9230b461ef011bf3
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-10-11 18:43:17 -0600

    Formatting.

--- FinalsSim/Program.cs
+++ FinalsSim/Program.cs
@@ -4,73 +4,79 @@
 // home court advantage. 
 using FinalsSim;
 
-List<int> result = new() { 0, 0, 0, 0, 0, 0, 0, 0 };
-
-string intro = "Calculating Series Odds...";
-Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (intro.Length / 2)) + "}", intro));
-Console.WriteLine();
-Console.WriteLine();
-
-// Get the probabilities and home/away teams from training file
-if (args.Length != 1)
-    throw new ArgumentException("Incrorect number of arguemnts");
-
-string[] pars = File.ReadAllLines(args[0]);
-
-// Predicted probability of team with homecourt advantage winning a home game. 
-// (derived from neural net and rounded)
-string HomeTeam = pars[0];
-double HomePercentage = Double.Parse(pars[1]);
-
-// Predicted probability of team without homecourt advantage winning a home game. 
-// (derived from neural net and rounded)
-string AwayTeam = pars[2];
-double AwayPercentage = Double.Parse(pars[3]);
-
-SeriesSim sim = new(AwayPercentage, HomePercentage);
-int runs = 10_000_000;
-
-// Run series simulation N times and count results
-for (int i = 0; i < runs; i++)
+internal class Program
 {
-    int index = sim.Run();
-    result[index]++;
+    private static void Main(string[] args)
+    {
+        List<int> result = new() { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        string intro = "Calculating Series Odds...";
+        Console.WriteLine(string.Format("{0," + (Console.WindowWidth / 2 + intro.Length / 2) + "}", intro));
+        Console.WriteLine();
+        Console.WriteLine();
+
+        // Get the probabilities and home/away teams from training file
+        if (args.Length != 1)
+            throw new ArgumentException("Incrorect number of arguemnts");
+
+        string[] pars = File.ReadAllLines(args[0]);
+
+        // Predicted probability of team with homecourt advantage winning a home game. 
+        // (derived from neural net and rounded)
+        string HomeTeam = pars[0];
+        double HomePercentage = double.Parse(pars[1]);
+
+        // Predicted probability of team without homecourt advantage winning a home game. 
+        // (derived from neural net and rounded)
+        string AwayTeam = pars[2];
+        double AwayPercentage = double.Parse(pars[3]);
+
+        SeriesSim sim = new(AwayPercentage, HomePercentage);
+        int runs = 10_000_000;
+
+        // Run series simulation N times and count results
+        for (int i = 0; i < runs; i++)
+        {
+            int index = sim.Run();
+            result[index]++;
+        }
+
+        double homeTeamProb = 0;
+        double awayTeamProb = 0;
+        double check = 0;
+
+        // add up probabilities and print results
+        for (int i = 0; i < result.Count; i++)
+        {
+            double prob = result[i] / (double)runs;
+            string winner = i < 4 ? HomeTeam : AwayTeam;
+            int games = i < 4 ? i + 4 : i;
+            check += prob;
+            Console.WriteLine($"\tThere is a(n) {prob * 100:F1} percent chance of the {winner} winning the series in {games} games.");
+            Console.WriteLine();
+
+            if (i < 4)
+                homeTeamProb += result[i];
+            else awayTeamProb += result[i];
+        }
+
+        homeTeamProb /= runs;
+        awayTeamProb /= runs;
+
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        string outro1 = "****************** Final Odds ******************";
+        string outro2 = "************************************************";
+
+        Console.WriteLine(string.Format("{0," + (Console.WindowWidth / 2 + outro1.Length / 2) + "}", outro1));
+        Console.WriteLine(string.Format("{0," + (Console.WindowWidth / 2 + outro2.Length / 2) + "}", outro2));
+        Console.ResetColor();
+
+        Console.WriteLine();
+        Console.WriteLine($"\tThe {HomeTeam} have a(n) {homeTeamProb * 100:F1} percent chance of winning the series.");
+        Console.WriteLine();
+        Console.WriteLine($"\tThe {AwayTeam} have a(n) {awayTeamProb * 100:F1} percent chance of winning the series.");
+        Console.WriteLine();
+    }
 }
-
-double homeTeamProb = 0;
-double awayTeamProb = 0;
-double check = 0;
-
-// add up probabilities and print results
-for (int i = 0; i < result.Count; i++)
-{
-    double prob = (result[i] / (double)runs);
-    string winner = (i < 4) ? HomeTeam : AwayTeam;
-    int games = (i < 4) ? i + 4 : i;
-    check += prob;
-    Console.WriteLine($"\tThere is a(n) {prob * 100:F1} percent chance of the {winner} winning the series in {games} games.");
-    Console.WriteLine();
-
-    if(i < 4)
-        homeTeamProb += result[i];
-    else awayTeamProb += result[i];
-}
-
-homeTeamProb /= runs;
-awayTeamProb /= runs;
-
-Console.WriteLine();
-
-Console.ForegroundColor = ConsoleColor.Green;
-string outro1 = "****************** Final Odds ******************";
-string outro2 = "************************************************";
-
-Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (outro1.Length / 2)) + "}", outro1));
-Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (outro2.Length / 2)) + "}", outro2));
-Console.ResetColor();
-
-Console.WriteLine();
-Console.WriteLine($"\tThe {HomeTeam} have a(n) {homeTeamProb * 100:F1} percent chance of winning the series.");
-Console.WriteLine();
-Console.WriteLine($"\tThe {AwayTeam} have a(n) {awayTeamProb * 100:F1} percent chance of winning the series.");
-Console.WriteLine();
--- FinalsSim/SeriesSim.cs
+++ FinalsSim/SeriesSim.cs
@@ -1,87 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿namespace FinalsSim;
 
-namespace FinalsSim
+/// <summary>
+/// This class runs a 7-gane nba series simulation.
+/// </summary>
+internal class SeriesSim
 {
+    public double AwayProb { get; private set; }
+    public double HomeProb { get; private set; }
+
     /// <summary>
-    /// This class runs a 7-gane nba series simulation.
+    /// Constructor
     /// </summary>
-    internal class SeriesSim
+    /// <param name="awayProb"></param>
+    /// <param name="homeProb"></param>
+    public SeriesSim(double awayProb, double homeProb)
     {
-        public double AwayProb { get; private set; }
-        public double HomeProb { get; private set; }
-        
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="awayProb"></param>
-        /// <param name="homeProb"></param>
-        public SeriesSim(double awayProb, double homeProb)
-        {
-            AwayProb = awayProb;
-            HomeProb = homeProb;
-        }
+        AwayProb = awayProb;
+        HomeProb = homeProb;
+    }
 
-        /// <summary>
-        /// Runs a 7 game series  
-        /// 
-        /// Returns the index of the result in an 8 element array containing all possible
-        /// outcomes.
-        /// </summary>
-        /// <returns></returns>
-        public int Run()
-        {
-            // Reset series wins
-            int homeWins = 0;
-            int awayWins = 0;
+    /// <summary>
+    /// Runs a 7 game series  
+    /// 
+    /// Returns the index of the result in an 8 element array containing all possible
+    /// outcomes.
+    /// </summary>
+    /// <returns></returns>
+    public int Run()
+    {
+        // Reset series wins
+        int homeWins = 0;
+        int awayWins = 0;
 
-            // Run the first four games
+        // Run the first four games
+        gameSim('h', ref homeWins, ref awayWins);
+        gameSim('h', ref homeWins, ref awayWins);
+        gameSim('a', ref homeWins, ref awayWins);
+        gameSim('a', ref homeWins, ref awayWins);
+
+        // Run Next three games if necessary
+        if (homeWins != 4 && awayWins != 4)
             gameSim('h', ref homeWins, ref awayWins);
+        if (homeWins != 4 && awayWins != 4)
+            gameSim('a', ref homeWins, ref awayWins);
+        if (homeWins != 4 && awayWins != 4)
             gameSim('h', ref homeWins, ref awayWins);
-            gameSim('a', ref homeWins, ref awayWins);
-            gameSim('a', ref homeWins, ref awayWins);
 
-            // Run Next three games if necessary
-            if (homeWins != 4 && awayWins != 4)
-                gameSim('h', ref homeWins, ref awayWins);
-            if (homeWins != 4 && awayWins != 4)
-                gameSim('a', ref homeWins, ref awayWins);
-            if (homeWins != 4 && awayWins != 4)
-                gameSim('h', ref homeWins, ref awayWins);
+        if (homeWins > awayWins)
+            return homeWins + awayWins - 4;
 
-            if (homeWins > awayWins)
-                return homeWins + awayWins - 4;
-            
-            return homeWins + awayWins;
-        }
+        return homeWins + awayWins;
+    }
 
-        /// <summary>
-        /// Predicts the result of a single game using the field probabilities. 
-        /// User must specify type of game('h' or 'a' for home or away).
-        /// </summary>
-        /// <param name="type"></param>
-        private void gameSim(char type, ref int homeWins, ref int awayWins)
+    /// <summary>
+    /// Predicts the result of a single game using the field probabilities. 
+    /// User must specify type of game('h' or 'a' for home or away).
+    /// </summary>
+    /// <param name="type"></param>
+    private void gameSim(char type, ref int homeWins, ref int awayWins)
+    {
+        Random rng = new();
+        double predict = rng.NextDouble();
+
+        if (type == 'h')
         {
-            Random rng = new();
-            double predict = rng.NextDouble();
-
-            if (type == 'h')
-            { 
-                if (predict <= HomeProb)
-                    homeWins++;
-                else awayWins++;
-            }
-            if(type == 'a')
-            {
-                if (predict <= AwayProb)
-                    awayWins++;
-                else homeWins++;
-            }
+            if (predict <= HomeProb)
+                homeWins++;
+            else awayWins++;
+        }
+        if (type == 'a')
+        {
+            if (predict <= AwayProb)
+                awayWins++;
+            else homeWins++;
         }
     }
 }
